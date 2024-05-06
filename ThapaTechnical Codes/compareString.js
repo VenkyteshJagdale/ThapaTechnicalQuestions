970c28b5ad96fd15d817aaf7c0cc13b7b3fd1aa8
JagdaleVenkytesh <venkytesh@digialpha.co>
2024-05-06 12:18:35 +0530

    find largest word from string.

---
+++ ThapaTechnical Codes/compareString.js
@@ -0,0 +1,32 @@
+// find logenst word from string
+
+//1).
+// let string = "This is javascript code by thapathechical and javascriptCommunity"
+// let output=(string) =>{
+// word = string.split(" ")
+// word = word.sort((a,b)=> b.length-a.length)
+// return word[0]
+// }
+
+// console.log(output(string))
+
+
+// 2).
+// let string = "This is javascript code by thapathechical and javascriptCommunity"
+// let output = (string)=>{
+//   word = string.split(" ")
+//   word = word.sort((a,b)=> a.length - b.length)
+//   console.log("largest word from string",word)
+//   return word.at(-1)
+// }
+// console.log(output(string))
+
+
+//3).
+// let string = "This is javascript code by thapathechical and javascriptCommunity"
+// let output = (string)=>{
+//   word = string.split(" ");
+//   return word.reduce((accu,curunt)=> accu.length > curunt.length ? accu : curunt)
+// }
+
+// console.log(output(string))
