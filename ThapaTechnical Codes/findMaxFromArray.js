e0e546a02800553f997df3e06929b8c24f9fc0ab
JagdaleVenkytesh <venkytesh@digialpha.co>
2024-05-06 15:05:18 +0530

    find maximum number from array

---
+++ ThapaTechnical Codes/findMaxFromArray.js
@@ -0,0 +1,8 @@
+// fIND MAXIMUM NUMBER FROM ARRAY
+
+let arr = [-10,-9,-3,-4,-2,-1,-110]
+// arr.reduce((cur,pre)=> {
+//   return cur>pre ? cur :pre
+// })
+maxNumber = Math.max(...arr)
+console.log(maxNumber)
