f1d75822e88a4f32342e51d72ac723b05cc2369d
JagdaleVenkytesh <venkytesh@digialpha.co>
2024-05-07 11:42:41 +0530

    Addition of array elemnts

---
+++ ThapaTechnical Codes/sumOfArrayElement.js
@@ -0,0 +1,26 @@
+// SUM of array element
+
+let arr = [1,2,3,4,5]
+
+let output = (arr) => {
+ return arr.reduce((cur,pre)=>{
+ return cur + pre
+})
+
+
+}
+console.log(output(arr))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
