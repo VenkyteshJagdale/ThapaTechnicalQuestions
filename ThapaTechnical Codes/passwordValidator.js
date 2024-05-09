dd01e8d4968612b0fabac7ae16efddc49f39592c
JagdaleVenkytesh <venkytesh@digialpha.co>
2024-05-09 17:35:52 +0530

    Password validator

---
+++ ThapaTechnical Codes/passwordValidator.js
@@ -0,0 +1,29 @@
+//Simple Password validator
+// using ascci  value
+
+let password = "Absdffscd1"
+let output = (pwd) => {
+let hasLowerCase = false;
+let hasUpperCase = false;
+let hasNumber = false;
+
+for(let char of pwd){
+if(char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90){
+hasLowerCase = true
+} else if(char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) {
+hasUpperCase = true
+}else if(!isNaN(Number(char))){
+hasNumber = true
+}
+}
+
+if(!hasLowerCase || !hasUpperCase || !hasNumber || pwd.length < 8){
+return false
+}
+
+return true
+
+}
+
+
+console.log(output(password))
